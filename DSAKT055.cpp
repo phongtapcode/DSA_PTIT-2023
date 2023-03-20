739d776ebb3246dcab6263cd03a3b55c530fc528
Phong Ba, Nguyen <110451287+phongtapcode@users.noreply.github.com>
2023-03-21 00:23:46 +0700

    Add files via upload

---
+++ DSAKT055.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--) {
+		int n,S;
+		cin>>n>>S;
+		int w[n+1], v[n+1];
+		for(int i=1; i<=n; i++)cin>>w[i];
+		for(int i=1; i<=n; i++)cin>>v[i];
+		int dp[n+1][S+1];
+		memset(dp,0,sizeof(dp));
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=S; j++) {
+				dp[i][j]=dp[i-1][j];
+				if(j>=w[i]) {
+					dp[i][j]=max(dp[i][j],dp[i-1][j-w[i]]+v[i]);
+				}
+			}
+		}
+		cout<<dp[n][S]<<endl;
+	}
+}
+//7 22
+//10 3 22 50 83 16 41
+//99 97 54 19 50 70 48
