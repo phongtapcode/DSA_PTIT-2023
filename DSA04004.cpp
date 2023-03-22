592cc559b23136ca8d9189432b7067d83faa7fcf
Phong Ba, Nguyen <110451287+phongtapcode@users.noreply.github.com>
2023-03-22 17:50:17 +0700

    Add files via upload

---
+++ DSA04004.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,k;
+		cin>>n>>k;
+		int i=2;
+		int cnt=0;
+		while(k%i!=i/2 && cnt<n*n){
+			i*=2;
+			cnt++;
+		}
+		cout<<cnt+1<<endl;
+	}
+}
+
